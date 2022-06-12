b6660025fc626b75673792f071dbdee2d4004539
James Winston <102254727+j-winston@users.noreply.github.com>
2022-06-12 07:06:07 -0400

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-"""Nutrionix Authentication"""
-NUTRIX_APP_ID = "3a1ba43a"
-NUTRIX_API_KEY = "5463a600baa85a1c482522e43ab3a295"
-
-"""Sheety Authentication"""
-SHEETY_AUTH_HEADER = "Basic a2p3aW5zdG9uOmJhbGxzdG90aGVjaGlu"
-SHEETY_USERNAME = "kjwinston"
-SHEETY_SPREADSHEET = "1fdcf02c06e0765faaf4be8b9840dd16"
